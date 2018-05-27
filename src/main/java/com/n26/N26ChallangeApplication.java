500e533f3af5a85a5fc6670966f3dcc8a115cb73
taimur.dev@gmail.com <bitrtpl@123>
2018-05-28 01:35:08 +0500

    remove unused annotation and import

--- src/main/java/com/n26/N26ChallangeApplication.java
+++ src/main/java/com/n26/N26ChallangeApplication.java
@@ -5,7 +5,6 @@ import java.util.concurrent.Executor;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 
 /**
@@ -13,7 +12,6 @@ import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
  */
 
 @SpringBootApplication
-@EnableAsync
 public class N26ChallangeApplication {
 
 	public static void main(String[] args) {
